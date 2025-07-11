adbdbadfedc63e43773e902d3ef477d22e01ce58
Sarthak Dhembare <165639215+SarthakTD@users.noreply.github.com>
2025-07-11 11:57:41 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,102 +1,51 @@
-import React from "react";
-import { personalData } from "../utils/data/personal-data";
-import { profileSectionSkillsData } from "../utils/data/skills";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
-import { MdDownload } from "react-icons/md";
-import { RiContactsFill } from "react-icons/ri";
-import { SiLeetcode } from "react-icons/si";
-import heroImage from "../assets/hero.svg"; // Make sure this exists in /src/assets
+import { motion } from "framer-motion";
 
-const HeroSection = () => {
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
   return (
-    <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
-      <img
-        src={heroImage}
-        alt="Hero"
-        className="absolute -top-[98px] -z-10 w-full object-cover"
-      />
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8 z-10">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello, <br />
-            This is <span className="text-pink-500">{personalData.name}</span>
-            {`, I'm a Professional `}
-            <span className="text-[#16f2b3]">{personalData.designation}</span>.
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Sarthak</span>
           </h1>
-
-          <div className="my-12 flex items-center gap-5">
-            <a href={personalData.github} target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-125 transition-all duration-300">
-              <BsGithub size={30} />
-            </a>
-            <a href={personalData.linkedIn} target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-125 transition-all duration-300">
-              <BsLinkedin size={30} />
-            </a>
-            <a href={personalData.facebook} target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-125 transition-all duration-300">
-              <FaFacebook size={30} />
-            </a>
-            <a href={personalData.leetcode} target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-125 transition-all duration-300">
-              <SiLeetcode size={30} />
-            </a>
-            <a href={personalData.twitter} target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-125 transition-all duration-300">
-              <FaTwitterSquare size={30} />
-            </a>
-          </div>
-
-          <div className="flex items-center gap-3">
-            <a href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full text-white font-semibold uppercase tracking-wider flex items-center gap-1 hover:gap-3">
-                <span>Contact me</span>
-                <RiContactsFill size={16} />
-              </button>
-            </a>
-
-            <a
-              className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-white font-semibold uppercase tracking-wider transition-all"
-              role="button"
-              target="_blank"
-              rel="noreferrer"
-              href={personalData.resume}
-            >
-              <span>Get Resume</span>
-              <MdDownload size={16} />
-            </a>
-          </div>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop Software <br className='sm:block hidden' />
+           and web applications
+          </p>
         </div>
+      </div>
 
-        <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
-          <div className="flex flex-row">
-            <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
-            <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
+   
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
           </div>
-          <div className="px-4 lg:px-8 py-5">
-            <div className="flex flex-row space-x-2">
-              <div className="h-3 w-3 rounded-full bg-red-400"></div>
-              <div className="h-3 w-3 rounded-full bg-orange-400"></div>
-              <div className="h-3 w-3 rounded-full bg-green-200"></div>
-            </div>
-          </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-            <code className="font-mono text-xs md:text-sm lg:text-base">
-              <div><span className="text-pink-500">const</span> <span className="text-white">coder</span> <span className="text-pink-500">=</span> <span className="text-gray-400">{'{'}</span></div>
-              <div className="ml-4 text-white">name: <span className="text-amber-300">'{personalData.name}'</span>,</div>
-              <div className="ml-4 text-white">
-                skills: [<span className="text-amber-300">{profileSectionSkillsData.map((skill, index) =>
-                  `'${skill}'${index !== profileSectionSkillsData.length - 1 ? ', ' : ''}`
-                )}</span>],
-              </div>
-              <div className="ml-4 text-white">hardWorker: <span className="text-orange-400">true</span>,</div>
-              <div className="ml-4 text-white">quickLearner: <span className="text-orange-400">true</span>,</div>
-              <div className="ml-4 text-white">problemSolver: <span className="text-orange-400">true</span>,</div>
-              <div className="ml-4 text-green-400">hireable: <span className="text-orange-400">function</span>() <span className="text-gray-400">{'{ return (this.hardWorker && this.problemSolver && this.skills.length >= 5); }'}</span></div>
-              <div><span className="text-gray-400">{'};'}</span></div>
-            </code>
-          </div>
-        </div>
+        </a>
       </div>
     </section>
   );
 };
 
-export default HeroSection;
+export default Hero;
