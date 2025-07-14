3cefc955a94776b8e4dcf370c579c45ac2780376
Sarthak Dhembare <165639215+SarthakTD@users.noreply.github.com>
2025-07-14 14:28:20 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,13 +19,49 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Sarthak</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-           Web Developer And React Developer <br className='sm:block hidden' />
-          
+            Web Developer And React Developer <br className='sm:block hidden' />
+
           </p>
         </div>
       </div>
+    
+      <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
+        <div className="flex flex-row">
+          <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
+          <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
+        </div>
+        <div className="px-4 lg:px-8 py-5">
+          <div className="flex flex-row space-x-2">
+            <div className="h-3 w-3 rounded-full bg-red-400"></div>
+            <div className="h-3 w-3 rounded-full bg-orange-400"></div>
+            <div className="h-3 w-3 rounded-full bg-green-200"></div>
+          </div>
+        </div>
+        <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+          <code className="font-mono text-xs md:text-sm lg:text-base text-white">
+            <div>
+              <span className="text-pink-500">const</span> <span className="text-white">coder</span> = <span className="text-gray-400">{'{'}</span>
+            </div>
+            <div className="ml-4 text-white">
+              name: <span className="text-amber-300">'{personalData.name}'</span>,
+            </div>
+            <div className="ml-4 text-white">
+              skills: [<span className="text-amber-300">
+                {profileSectionSkillsData.map((skill, i) => `'${skill}'${i !== profileSectionSkillsData.length - 1 ? ', ' : ''}`)}
+              </span>],
+            </div>
+            <div className="ml-4 text-white">hardWorker: <span className="text-orange-400">true</span>,</div>
+            <div className="ml-4 text-white">quickLearner: <span className="text-orange-400">true</span>,</div>
+            <div className="ml-4 text-white">problemSolver: <span className="text-orange-400">true</span>,</div>
+            <div className="ml-4 text-green-400">
+              hireable: <span className="text-orange-400">function</span>() <span className="text-white">{'{ return (this.hardWorker && this.problemSolver && this.skills.length >= 5); }'}</span>
+            </div>
+            <div><span className="text-white">{'};'}</span></div>
+          </code>
+        </div>
+      </div>
 
-   
+
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
